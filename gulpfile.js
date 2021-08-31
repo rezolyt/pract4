de418b2b77e3bd37068dc089f0a98e8660986dd4
rezolyt <rezolyt@gmail.com>
2021-08-31 17:41:23 +0300

    commit

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass')(require('sass'));
+
+function buildStyles() {
+  return gulp.src('./sass/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+};
+
+exports.buildStyles = buildStyles;
